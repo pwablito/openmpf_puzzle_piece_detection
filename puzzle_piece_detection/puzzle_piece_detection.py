a4bc2b973f9b5b7e671f5046b9efe5cc43226f9d
Spencer, Paul Whiting <pwablito@gmail.com>
2025-01-05 14:57:24 -0500

    Full video implementation

--- puzzle_piece_detection/puzzle_piece_detection.py
+++ puzzle_piece_detection/puzzle_piece_detection.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import logging
+import time
 
 import mpf_component_api as mpf
 import mpf_component_util as mpf_util
@@ -14,11 +15,19 @@ def load_model():
 class PuzzlePieceDetection(mpf_util.VideoCaptureMixin, mpf_util.ImageReaderMixin):
 
     def __init__(self):
+        self.model_loaded = False
         self.init_model()
 
     def init_model(self):
+        self.model_loaded = False
         self.model = load_model()
+        self.model_loaded = True
 
+    def wait_for_model_to_load(self):
+        while not self.model_loaded:
+            logger.info('Waiting for model to load...')
+            time.sleep(1)
+        logger.info('Model loaded')
 
     @staticmethod
     def yolo_result_to_mpf_image_location(result):
@@ -37,12 +46,52 @@ class PuzzlePieceDetection(mpf_util.VideoCaptureMixin, mpf_util.ImageReaderMixin
 
     def get_detections_from_video_capture(self, video_job, video_capture):
         logger.info('[%s] Received video job: %s', video_job.job_name, video_job)
-        self.init_model()  # Needed to reset track ids
+        self.init_model()
+        self.wait_for_model_to_load()
+        # self.model.predictor.trackers[0].reset()  # Needed to reset track ids
+        tracked_objects = defaultdict(list)
+        for frame_index, frame in enumerate(video_capture):
+            results = self.model.track(
+                frame,
+                persist=True,
+                conf=float(video_job.job_properties["CONFIDENCE"])
+            )[0]
+            for result in results:
+                if not result.boxes.is_track:
+                    logger.info(f'Skipping non-track result in frame {frame_index}')
+                    continue  # Sometimes the first detection of a track is not assigned a track id. If this happens, ignore it
+                image_location = PuzzlePieceDetection.yolo_result_to_mpf_image_location(result)
+                # print(image_location)
+                object_id = int(result.boxes.id)
+                tracked_objects[object_id].append({
+                    "location": image_location,
+                    "frame": frame_index,
+                })
         tracks = []
+        for locations in tracked_objects.values():
+            class_name = locations[0]["location"].detection_properties["CLASSIFICATION"]
+            start_frame = locations[0]["frame"]
+            stop_frame = locations[-1]["frame"]
+            max_confidence = max([location["location"].confidence for location in locations])
+            tracks.append(
+                mpf.VideoTrack(
+                    start_frame,
+                    stop_frame,
+                    max_confidence,
+                    frame_locations={
+                        location["frame"]: location["location"] for location in locations
+                    },
+                    detection_properties={
+                        "CLASSIFICATION": class_name,
+                    }
+                )
+            )
         return tracks
 
     def get_detections_from_image_reader(self, image_job, image_reader):
         logger.info('[%s] Received image job: %s', image_job.job_name, image_job)
+        self.init_model()
+        self.wait_for_model_to_load()
         results = self.model(
             image_reader.get_image(),
             conf=float(image_job.job_properties["CONFIDENCE"])
