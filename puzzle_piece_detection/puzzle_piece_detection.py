6096db16a9e0b7c1f81b0c4f96ae626bec60cfd2
Spencer, Paul Whiting <pwablito@gmail.com>
2025-01-04 22:07:33 -0500

    Working image processor

--- puzzle_piece_detection/puzzle_piece_detection.py
+++ puzzle_piece_detection/puzzle_piece_detection.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import logging
 
 import mpf_component_api as mpf
@@ -13,16 +14,40 @@ def load_model():
 class PuzzlePieceDetection(mpf_util.VideoCaptureMixin, mpf_util.ImageReaderMixin):
 
     def __init__(self):
+        self.init_model()
+
+    def init_model(self):
         self.model = load_model()
+
+
+    @staticmethod
+    def yolo_result_to_mpf_image_location(result):
+        class_name = result.names[int(result.boxes.cls)]
+        confidence = result.boxes.conf[0]
+        x1, y1, x2, y2 = result.boxes.xyxy[0]
+        width = x2 - x1
+        height = y2 - y1
+        return mpf.ImageLocation(
+            x1, y1, width, height, confidence, {
+                "CLASSIFICATION": class_name,
+                "CLASSIFICATION CONFIDENCE LIST": f"{confidence}",
+                "CLASSIFICATION LIST": f"{class_name}"
+            }
+        )
 
     def get_detections_from_video_capture(self, video_job, video_capture):
         logger.info('[%s] Received video job: %s', video_job.job_name, video_job)
-        results = self.model(video_capture)
-        logger.info(results)
-        return []
+        self.init_model()  # Needed to reset track ids
+        tracks = []
+        return tracks
 
     def get_detections_from_image_reader(self, image_job, image_reader):
         logger.info('[%s] Received image job: %s', image_job.job_name, image_job)
-        results = self.model(image_reader.get_image())
-        logger.info(results)
-        return []
+        results = self.model(
+            image_reader.get_image(),
+            conf=float(image_job.job_properties["CONFIDENCE"])
+        )[0]
+        detections = []
+        for result in results:
+            detections.append(PuzzlePieceDetection.yolo_result_to_mpf_image_location(result))
+        return detections
